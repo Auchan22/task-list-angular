1b5de58c40a16f0d213e6e292dc6bf602a0eb981
Agustin <agustinsurila@gmail.com>
2023-02-14 17:59:34 -0300

    Styles applied for task list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { AddTaskBtnComponent } from './components/add-task-btn/add-task-btn.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddTaskBtnComponent
+    AddTaskBtnComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
